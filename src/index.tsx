7d52bda3fd8b23f244c321a71c8e5233c8a7db71
Jimbo <rodionminaev96@gmail.com>
2022-08-30 08:19:36 +0300

    fix css

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,13 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store, { persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
-import App from './app';
-import { GlobalStyle } from './styles';
+import './styles/index.css';
 
+import App from './app';
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <GlobalStyle />
       <App />
     </PersistGate>
   </Provider>,
